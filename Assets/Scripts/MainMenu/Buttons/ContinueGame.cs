9c104bbbca4dfb99ac3469305271c45fd214cfe0
CodyMarkix <Markix124@protonmail.com>
2023-10-15 22:44:28 +0200

    urgent fix: continue buton sending to menu scene

--- Assets/Scripts/MainMenu/Buttons/ContinueGame.cs
+++ Assets/Scripts/MainMenu/Buttons/ContinueGame.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ContinueGame : MonoBehaviour {
     public void OnButtonPress() {
+        if (!PlayerPrefs.HasKey("night") || PlayerPrefs.GetInt("night") == 0) {
+            PlayerPrefs.SetInt("night", 1);
+        }
+
         if (PlayerPrefs.GetInt("night") <= 5) {
             SceneManager.LoadScene(PlayerPrefs.GetInt("night") + 1);
             Debug.Log(PlayerPrefs.GetInt("night"));
